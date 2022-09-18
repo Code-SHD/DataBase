d467cf494a8ef84174738c5751c6d4cfb70273ed
Code.SHD <98087876+Code-SHD@users.noreply.github.com>
2022-09-18 12:48:16 +0900

    Add files via upload

---
+++ database-1.py
@@ -0,0 +1,39 @@
+class DataBase():
+    def __init__(self):
+        self.key = []
+        self.value = []
+        
+    def return_key(self, value):
+        for i in range(len(self.value)):
+            if self.value[i] == value:
+                return self.key[i]
+            else:
+                pass
+    
+    def return_value(self, key):
+        for j in range(len(self.key)):
+            if self.key[j] == key:
+                return self.value[j]
+            else:
+                pass
+    
+    def return_key_and_value(self, num):
+        try:
+            return self.key[num-1], self.value[num-1]
+        except:
+            return "None"
+    
+    def add(self, key, value):
+        self.key.append(key)
+        self.value.append(value)
+    
+    def delete(self, num):
+        del self.key[num]
+        del self.value[num]
+        
+DB = DataBase()
+DB.add("a", 10)
+DB.add("b", 20)
+DB.add("c", 30)
+
+print(DB.return_key_and_value(3))
